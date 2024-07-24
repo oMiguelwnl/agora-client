e4b52e2774e06d5258603eebdfbf09a7bbb6360e
oMiguelwnl <contato.Miguel.almeida7@gmail.com>
2024-07-24 10:51:57 -0300

    last Part

--- app/components/Admin/Analytics/CourseAnalytics.tsx
+++ app/components/Admin/Analytics/CourseAnalytics.tsx
@@ -17,6 +17,16 @@ type Props = {};
 const CourseAnalytics = (props: Props) => {
   const { data, isLoading } = useGetCoursesAnalyticsQuery({});
 
+  // const analyticsData = [
+  //     { name: 'Jun 2023', uv: 3 },
+  //     { name: 'July 2023', uv: 2 },
+  //     { name: 'August 2023', uv: 5 },
+  //     { name: 'Sept 2023', uv: 7 },
+  //     { name: 'October 2023', uv: 2 },
+  //     { name: 'Nov 2023', uv: 5 },
+  //     { name: 'December 2023', uv: 7 },
+  //   ];
+
   const analyticsData: any = [];
 
   data &&
