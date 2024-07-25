538cca1b530b4240e88edf7260ba381f3773cf1b
oMiguelwnl <contato.Miguel.almeida7@gmail.com>
2024-07-25 13:05:52 -0300

    updating the texts to Portuguese

--- app/components/Route/Reviews.tsx
+++ app/components/Route/Reviews.tsx
@@ -11,7 +11,7 @@ export const reviews = [
     avatar: "https://randomuser.me/api/portraits/men/1.jpg",
     profession: "Estudante | Universidade de Cambridge",
     comment:
-      "Tive o prazer de explorar o E-learning, um site que oferece uma extensa gama de cursos sobre vários tópicos relacionados a tecnologia. Fiquei profundamente impressionado com minha experiência, pois o site oferece uma seleção abrangente de cursos que atendem a diferentes níveis de habilidade e interesses. Se você está procurando aprimorar seu conhecimento e habilidades na indústria de tecnologia, recomendo muito conferir o E-learning!",
+      "Tive o prazer de explorar o Ágora, um site que oferece uma extensa gama de cursos sobre vários tópicos relacionados a tecnologia. Fiquei profundamente impressionado com minha experiência, pois o site oferece uma seleção abrangente de cursos que atendem a diferentes níveis de habilidade e interesses. Se você está procurando aprimorar seu conhecimento e habilidades na indústria de tecnologia, recomendo muito conferir o Ágora!",
   },
   {
     name: "Verna Santos",
@@ -32,7 +32,7 @@ export const reviews = [
     avatar: "https://randomuser.me/api/portraits/women/2.jpg",
     profession: "Desenvolvedora Web Júnior | Indonésia",
     comment:
-      "Tive o prazer de explorar o E-learning, um site que oferece uma extensa gama de cursos sobre vários tópicos relacionados a tecnologia. Fiquei profundamente impressionado com minha experiência.",
+      "Tive o prazer de explorar o Ágora, um site que oferece uma extensa gama de cursos sobre vários tópicos relacionados a tecnologia. Fiquei profundamente impressionado com minha experiência.",
   },
   {
     name: "Rosemary Smith",
@@ -46,7 +46,7 @@ export const reviews = [
     avatar: "https://randomuser.me/api/portraits/women/4.jpg",
     profession: "Desenvolvedora Full Stack | Canadá",
     comment:
-      "Junte-se ao E-learning! O E-learning foca em aplicações práticas, em vez de apenas ensinar a teoria por trás das linguagens de programação ou frameworks. Fiz uma aula sobre a criação de um marketplace web usando React JS, e foi muito útil para me ensinar as diferentes etapas envolvidas na criação de um projeto do início ao fim. No geral, recomendo muito o E-learning para quem deseja melhorar suas habilidades de programação e construir projetos práticos. O E-learning é um ótimo recurso que ajudará você a levar suas habilidades para o próximo nível.",
+      "Junte-se ao Ágora! O Ágora foca em aplicações práticas, em vez de apenas ensinar a teoria por trás das linguagens de programação ou frameworks. Fiz uma aula sobre a criação de um marketplace web usando React JS, e foi muito útil para me ensinar as diferentes etapas envolvidas na criação de um projeto do início ao fim. No geral, recomendo muito o Ágora para quem deseja melhorar suas habilidades de programação e construir projetos práticos. O Ágora é um ótimo recurso que ajudará você a levar suas habilidades para o próximo nível.",
   },
 ];
 
